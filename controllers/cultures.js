d3d0dc8f3c68c4ed3bafbaa2900c6fd0a61a3a32
NatNichols <natalie.e.nichols@gmail.com>
2025-02-03 21:16:59 -0500

    create cultures show ctrl fn

--- controllers/cultures.js
+++ controllers/cultures.js
@@ -3,7 +3,17 @@ import { Culture } from '../models/culture.js'
 export async function index(req, res) {
   try {
     const cultures = await Culture.find({})
-    res.json(cultures)
+    res.status(200).json(cultures)
+  } catch (err) {
+    console.log(`🚨`, err)
+    res.status(500).json(`🚨`, err)
+  }
+}
+
+export async function show(req, res) {
+  try {
+    const culture = await Culture.findById(req.params.cultureId)
+    res.status(200).json(culture)
   } catch (err) {
     console.log(`🚨`, err)
     res.status(500).json(`🚨`, err)
