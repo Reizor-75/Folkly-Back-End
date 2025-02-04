f8e60c7e33c9eb5d866049837da89053bdec0037
mlinares <m.linares93@gmail.com>
2025-02-03 20:42:03 -0500

    create lesson schema, add list of lessons to culture model

--- models/culture.js
+++ models/culture.js
@@ -2,6 +2,20 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const lessonSchema= new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  lessonType:{
+    type: String,
+    enum: ['Movies','Music','Televison', 'Literature', 'Traditions'],
+    default: 'Movies'
+  },
+  correctAnswer: { type: String, required: true },
+  incorrectAnswer: [{ type: String, required: true }]
+}, {
+  timestamps: true
+})
+
 const culutreSchema = new Schema({
   name: { type: String, required: true },
   lang: { type: String, required: true },
@@ -9,7 +23,8 @@ const culutreSchema = new Schema({
     type: String,
     enum: ['Movies','Music','Televison', 'Literature', 'Traditions'],
     default: 'Movies'
-  }]
+  }],
+  lessons: [lessonSchema],
 },{
   timestamps: true,
 })
